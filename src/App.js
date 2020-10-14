6618a2245cdd38b190010ea33223c6d3055c7a54
Course <superleogame@gmail.com>
2020-10-13 22:06:17 -0300

    Testes de rotas feitos em aula.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Link, Route} from "react-router-dom";
+import routesConfig from "./routesConfig";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <Link className="px-1" to="/">Home</Link>
+          <Link className="px-1" to="/user">User</Link>
+          <Link className="px-1" to="/login">Login</Link>
+        </nav>
       </header>
+      {routesConfig.map(
+        (value, key) => {
+          return <Route key={key} path={value.path} component={value.component} exact={value.exact}
+          ></Route>
+        }
+      )}
     </div>
   );
 }
